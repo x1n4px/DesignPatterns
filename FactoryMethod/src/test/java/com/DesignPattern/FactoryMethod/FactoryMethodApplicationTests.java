022beb091d41318c8e0c635d60f49bff81c60575
x1n4px <silosabia@gmail.com>
2025-06-02 13:15:00 +0200

    act

---
+++ FactoryMethod/src/test/java/com/DesignPattern/FactoryMethod/FactoryMethodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DesignPattern.FactoryMethod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactoryMethodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
